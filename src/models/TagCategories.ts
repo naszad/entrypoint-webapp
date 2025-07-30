558ff4988d9beb5936f68e47c183af7dbe88058d
Yasir Ali <yasir@conversotech.com>
2025-07-30 18:39:38 +0500

    tagging system models/table structure + migration scripts

---
+++ src/models/TagCategories.ts
@@ -0,0 +1,6 @@
+import { pgTable,  uuid, text, } from "drizzle-orm/pg-core";
+
+export const tagCategories = pgTable("tag_categories", {
+  tagCategoryId: uuid("tag_category_id").primaryKey().defaultRandom(),
+  name: text("name").unique().notNull(),
+});
