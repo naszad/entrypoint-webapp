b358f2cb375933b151c3c2932e841b316700c5c1
Caleb Francis <calebonesimus@users.noreply.github.com>
2025-10-20 12:47:48 -0400

    Increase file upload limit (#138)
    * Increase file upload limit
    
    * Increase file upload limit to 80MB

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: "80mb",
+    },
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "80mb",
+    },
+  },
 };
 
 export default nextConfig;
