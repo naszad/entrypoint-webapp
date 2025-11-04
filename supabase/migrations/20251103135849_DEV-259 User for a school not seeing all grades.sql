cbe1f95cf3e2a3d480333dd25ed58ea8389d0b5a
Junaid <junaid.khokhar@gamil.com>
2025-11-04 18:37:10 +0500

    incorporating feedback

--- supabase/migrations/20251103135849_DEV-259 User for a school not seeing all grades.sql
+++ supabase/migrations/20251103135849_DEV-259 User for a school not seeing all grades.sql
@@ -13,7 +13,7 @@ for select
 to authenticated
 using ((EXISTS ( SELECT 1
    FROM (user_school_memberships usm
-     CROSS JOIN LATERAL ( SELECT ( SELECT get_current_school_id() AS get_current_school_id) AS current_school_id) app_context)
+     CROSS JOIN LATERAL (SELECT get_current_school_id() AS current_school_id) app_context)
   WHERE ((usm.school_id = sections.school_id) AND (usm.user_id = ( SELECT auth.uid() AS uid)) AND ((app_context.current_school_id IS NULL) OR (sections.school_id = app_context.current_school_id))))));
 
 drop policy if exists "Allow Reading of Student Grades" on "public"."student_grades";
@@ -26,7 +26,7 @@ to authenticated
 using ((EXISTS ( SELECT 1
    FROM ((sections s
      JOIN user_school_memberships usm ON ((usm.school_id = s.school_id)))
-     CROSS JOIN LATERAL ( SELECT ( SELECT get_current_school_id() AS get_current_school_id) AS current_school_id) app_context)
+     CROSS JOIN LATERAL (SELECT get_current_school_id() AS current_school_id) app_context)
   WHERE ((s.section_id = student_grades.section_id) AND (usm.user_id = ( SELECT auth.uid() AS uid)) AND ((app_context.current_school_id IS NULL) OR (s.school_id = app_context.current_school_id))))));
 
 drop policy if exists "Allow Reading of Section Enrollments" on "public"."section_enrollments";
@@ -39,6 +39,6 @@ to authenticated
 using ((EXISTS ( SELECT 1
    FROM ((sections s
      JOIN user_school_memberships usm ON ((usm.school_id = s.school_id)))
-     CROSS JOIN LATERAL ( SELECT ( SELECT get_current_school_id() AS get_current_school_id) AS current_school_id) app_context)
+     CROSS JOIN LATERAL (SELECT get_current_school_id() AS current_school_id) app_context)
   WHERE ((s.section_id = section_enrollments.section_id) AND (usm.user_id = ( SELECT auth.uid() AS uid)) AND ((app_context.current_school_id IS NULL) OR (s.school_id = app_context.current_school_id))))));
 
