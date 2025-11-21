5de2ed21477acaab8cf0d5c11968855361d24578
Matt Rogers <rogersmj@gmail.com>
2025-11-21 10:27:03 -0500

    Upgrade to Next.js 16 (#198)
    * Package upgrades
    
    * Removing storybook and vitest until/unless we need them

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "80mb",
-    },
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: "80mb",
